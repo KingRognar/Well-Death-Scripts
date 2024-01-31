240cdff570d6d03edb5e6022f281281ba3298d0e
KingRognar <Kingrognar@gmail.com>
2024-01-31 03:57:44 +0300

    Ability systems work (day 5 - 31.01.2024)
    - added script for AbilityInterpreter
    - now abilityTargeting updates only when new position is given
    - layed some groundwork for interactions with AbilityInterpreter
    - added comments, TODOs and reorganized for later clarity

--- Systems/Ability Systems/AbilityTargeting_Scr.cs
+++ Systems/Ability Systems/AbilityTargeting_Scr.cs
@@ -6,17 +6,27 @@ using Abilities;
 
 public class AbilityTargeting_Scr : MonoBehaviour
 {
-    public LayerMask layerMask;
-    public Transform marker;
-    public List<Transform> markersList = new List<Transform>();
+    //-// Переменные типа прицеливания 
+    // TODO: потом просто запихнуть сюда scriptable object абилки
     public TargetingType targetingType;
     public AreaType _areaType;
     public AreaType areaType
     {
-        get {return _areaType;}
-        set {_areaType = value; OnAreaTypeChange(); }
+        get { return _areaType; }
+        set { _areaType = value; OnAreaTypeChange(); }
     }
     public int circleSize = 2;
+
+    //-// Переменные маркеров
+    public Transform marker;
+    private Vector3 previousMarkerPosition = Vector3.zero;
+    public List<Transform> markersList = new List<Transform>();
+
+    //-// Переменные для взаимодействия с другими скриптами
+    public List<Vector3Int> sendTargetsList = new List<Vector3Int>();
+
+    //-// Переменные рейкаста
+    public LayerMask layerMask;
     [Space(30)]
     public string objectName;
     public Vector3 hitPoint;
@@ -25,12 +35,30 @@ public class AbilityTargeting_Scr : MonoBehaviour
     bool kavo = true;
 
 
+    // TODO: подписать и прибраться в переменных
+
+
     private void Start()
     {
         OnAreaTypeChange();
     }
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            if (kavo)
+            {
+                areaType = AreaType.SingleCell;
+                kavo = !kavo;
+            }
+            else
+            {
+                areaType = AreaType.Circle;
+                kavo = !kavo;
+            }
+        }
+
+
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
@@ -42,34 +70,40 @@ public class AbilityTargeting_Scr : MonoBehaviour
             Vector3 markerPosition = hitPoint + hitNormal * 0.5f;
             markerPosition = new Vector3(Mathf.Round(markerPosition.x), Mathf.Round(markerPosition.y), Mathf.Round(markerPosition.z));
             // TODO: пропускать если основная цель в воздухе? (мб опционно)
-            areaHighlight(markerPosition); // TODO: можно запускать, только если позиция поменялась
-        }
-        
-        
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            if (kavo)
-            {
-                areaType = AreaType.SingleCell;
-                kavo = !kavo;
-            }    
-            else
-            {
-                areaType = AreaType.Circle;
-                kavo = !kavo;
-            }
-        }
+            if (markerPosition == previousMarkerPosition)
+                return;
+            previousMarkerPosition = markerPosition;
+            areaHighlight(markerPosition);
+            if (!Input.GetKeyDown(KeyCode.Mouse0))
+                return;
+            SendTargetsAndExecuteAbility();
+        }  
     }
 
     private Action<Vector3> areaHighlight;
-    private void SingleCellAreaHighlight(Vector3 targetPos)
+    /// <summary>
+    /// Метод для выделения цели - одной клеткой
+    /// (Используется с делегатом)
+    /// </summary>
+    /// <param name="targetPos">Позиция которую необходимо выделить</param>
+    private void SingleCellAreaHighlight(Vector3 targetPos) // TODO: доделать - RemovePositionsInAir
     {
+        sendTargetsList = new List<Vector3Int>();
+        sendTargetsList.Add(Field_Scr.WorldToMapPosition(targetPos));
         markersList[0].position = targetPos;
     }
-    private void CircleAreaHighlight(Vector3 targetPos)
+    /// <summary>
+    /// Метод для выделения цели - круговой областью
+    /// (Используется с делегатом)
+    /// </summary>
+    /// <param name="targetPos">Центр круговой области</param>
+    private void CircleAreaHighlight(Vector3 targetPos) // TODO: разделить на части и сделать модульным
     {
         List<Vector3> positions = CircleAreaPositions(targetPos);
-        positions = RemovePositionsInAir(positions);// TODO: сделать опциональным?
+        positions = RemoveIncorrectPositions(positions);// TODO: сделать опциональным?
+        sendTargetsList = new List<Vector3Int>();
+        foreach (Vector3 pos in positions)
+            sendTargetsList.Add(Field_Scr.WorldToMapPosition(pos));
         for (int i = 0; i < markersList.Count; i++)
         {
             if (i < positions.Count)
@@ -79,6 +113,11 @@ public class AbilityTargeting_Scr : MonoBehaviour
         }
 
     }
+    /// <summary>
+    /// Метод для нахождения позиций входящих в круг
+    /// </summary>
+    /// <param name="markerPos">Центр круговой области</param>
+    /// <returns>Лист полученных позиций</returns>
     List<Vector3> CircleAreaPositions (Vector3 markerPos)
     {
         List<Vector3> positions = new List<Vector3>();
@@ -91,13 +130,23 @@ public class AbilityTargeting_Scr : MonoBehaviour
         return positions;
     }
 
+    /// <summary>
+    /// Метод для нахождения Центрированного квадратного числа(ЦКЧ) n-ого порядка
+    /// </summary>
+    /// <param name="num">Порядок искомого ЦКЧ</param>
+    /// <returns>Искомое ЦКЧ</returns>
     int CenteredSqareNumber(int num)
     {
         num = Mathf.Max(num, 0);
         int ans = ((int)Mathf.Pow((2*num - 1), 2) + 1) / 2;
         return ans;
     }
-    List<Vector3> RemovePositionsInAir(List<Vector3> positions)
+    /// <summary>
+    /// Метод для удаления из листа неподохдящих позиций
+    /// </summary>
+    /// <param name="positions">Лист из которого необходимо удалить позиции</param>
+    /// <returns>Очищенный лист позиций</returns>
+    List<Vector3> RemoveIncorrectPositions(List<Vector3> positions)
     {
         List<Vector3> newPositions = new List<Vector3>();
         foreach (Vector3 pos in positions)
@@ -110,6 +159,10 @@ public class AbilityTargeting_Scr : MonoBehaviour
         }
         return newPositions;
     }
+    /// <summary>
+    /// Метод для создания\удаления необходимого количества префабов для текущего вида области
+    /// </summary>
+    /// <param name="count">Необходимое кол-во префабов в листе</param>
     void PrepareMarkers(int count)
     {
         while(markersList.Count != count)
@@ -125,6 +178,9 @@ public class AbilityTargeting_Scr : MonoBehaviour
             Destroy(objToRemove);
         }
     }
+    /// <summary>
+    /// Метод, вызываемый при изменений переменной areaType
+    /// </summary>
     void OnAreaTypeChange() // TODO: подумоть как оптимизировать
     {
         if (_areaType == AreaType.Circle)
@@ -137,5 +193,11 @@ public class AbilityTargeting_Scr : MonoBehaviour
             PrepareMarkers(1);
             areaHighlight = SingleCellAreaHighlight;
         }
+    }
+
+    private void SendTargetsAndExecuteAbility()
+    {
+        // TODO: отправляем полученные клетки в AbilityInterpreter
+        // TODO: запускаем в AbilityInterpreter'e выполнение абилки
     }
 }
