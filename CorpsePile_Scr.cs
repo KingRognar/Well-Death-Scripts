f6f5496be126d605119a295c95ab490bf857b924
KingRognar <Kingrognar@gmail.com>
2024-06-04 00:24:18 +0300

    Corpse Pile (day 19 - 04.06.24)
    - corpse pile mesh generation

--- CorpsePile_Scr.cs
+++ CorpsePile_Scr.cs
@@ -43,6 +43,8 @@ public class CorpsePile_Scr : MonoBehaviour
         pileMesh.uv = uv;
         pileMesh.triangles = triangles;
 
+        pileMesh.RecalculateNormals();
+
         transform.GetComponent<MeshFilter>().mesh = pileMesh;
         transform.GetComponent<MeshRenderer>().material = material;
     }
@@ -50,61 +52,41 @@ public class CorpsePile_Scr : MonoBehaviour
 
     private Vector3[] GenerateVertices()
     {
-        int totalVertCount = 9*meshSizeX*meshSizeZ + 3*meshSizeX + 3*meshSizeZ + 1;
+        int totalVertCount = 16 * meshSizeX * meshSizeZ;
         Vector3[] vertices = new Vector3[totalVertCount];
 
         for (int k = 0; k < meshSizeZ; k++)
         {
             for (int i = 0; i < meshSizeX; i++)
             {
-                OneCellVerticesGen(i, k).CopyTo(vertices, k*meshSizeX*9 + 3*k + 9*i);
+                OneCellVerticesGen(i, k).CopyTo(vertices, k * meshSizeX * 16 + i * 16);
             }
-            LastColumnVerticesGen(k).CopyTo(vertices, (k + 1)*meshSizeX*9 + 3*k);
         }
-        LastRowVerticesGen().CopyTo(vertices, meshSizeZ * (9 * meshSizeX + 3));
 
         return vertices;
-
     }
     private Vector3[] OneCellVerticesGen(int x, int z)
     {
-        Vector3[] cellVertices = new Vector3[9];
+        Vector3[] cellVertices = new Vector3[16];
 
         cellVertices[0] = new Vector3(x - 0.5f, 0, meshSizeZ - z - 0.5f);
         cellVertices[1] = new Vector3(x - 0.25f, 0, meshSizeZ - z - 0.5f);
         cellVertices[2] = new Vector3(x + 0.25f, 0, meshSizeZ - z - 0.5f);
-        cellVertices[3] = new Vector3(x - 0.5f, 0, meshSizeZ - z - 0.75f);
-        cellVertices[4] = new Vector3(x - 0.25f, 0, meshSizeZ - z - 0.75f);
-        cellVertices[5] = new Vector3(x + 0.25f, 0, meshSizeZ - z - 0.75f);
-        cellVertices[6] = new Vector3(x - 0.5f, 0, meshSizeZ - z - 1.25f);
-        cellVertices[7] = new Vector3(x - 0.25f, 0, meshSizeZ - z - 1.25f);
-        cellVertices[8] = new Vector3(x + 0.25f, 0, meshSizeZ - z - 1.25f);
+        cellVertices[3] = new Vector3(x + 0.5f, 0, meshSizeZ - z - 0.5f);
+        cellVertices[4] = new Vector3(x - 0.5f, 0, meshSizeZ - z - 0.75f);
+        cellVertices[5] = new Vector3(x - 0.25f, 0, meshSizeZ - z - 0.75f);
+        cellVertices[6] = new Vector3(x + 0.25f, 0, meshSizeZ - z - 0.75f);
+        cellVertices[7] = new Vector3(x + 0.5f, 0, meshSizeZ - z - 0.75f);
+        cellVertices[8] = new Vector3(x - 0.5f, 0, meshSizeZ - z - 1.25f);
+        cellVertices[9] = new Vector3(x - 0.25f, 0, meshSizeZ - z - 1.25f);
+        cellVertices[10] = new Vector3(x + 0.25f, 0, meshSizeZ - z - 1.25f);
+        cellVertices[11] = new Vector3(x + 0.5f, 0, meshSizeZ - z - 1.25f);
+        cellVertices[12] = new Vector3(x - 0.5f, 0, meshSizeZ - z - 1.5f);
+        cellVertices[13] = new Vector3(x - 0.25f, 0, meshSizeZ - z - 1.5f);
+        cellVertices[14] = new Vector3(x + 0.25f, 0, meshSizeZ - z - 1.5f);
+        cellVertices[15] = new Vector3(x + 0.5f, 0, meshSizeZ - z - 1.5f);
 
         return cellVertices;
-    }
-    private Vector3[] LastColumnVerticesGen(int z)
-    {
-        Vector3[] clmnVertices = new Vector3[3];
-
-        clmnVertices[0] = new Vector3(meshSizeX - 0.5f, 0, meshSizeZ - z - 0.5f);
-        clmnVertices[1] = new Vector3(meshSizeX - 0.5f, 0, meshSizeZ - z - 0.75f);
-        clmnVertices[2] = new Vector3(meshSizeX - 0.5f, 0, meshSizeZ - z - 1.25f);
-
-        return clmnVertices;
-    }
-    private Vector3[] LastRowVerticesGen()
-    {
-        Vector3[] rowVerices = new Vector3[meshSizeX * 3 + 1];
-
-        for (int i = 0; i < meshSizeX; i++)
-        {
-            rowVerices[i * 3] = new Vector3(i - 0.5f, 0, -0.5f);
-            rowVerices[i * 3 + 1] = new Vector3(i - 0.25f, 0, -0.5f);
-            rowVerices[i * 3 + 2] = new Vector3(i + 0.25f, 0, -0.5f);
-        }
-        rowVerices[rowVerices.Length - 1] = new Vector3(meshSizeX - 0.5f, 0, -0.5f);
-
-        return rowVerices;
     }
 
     private Vector2[] GenerateUV()
@@ -119,34 +101,48 @@ public class CorpsePile_Scr : MonoBehaviour
 
         return uv;
     }
+
     private int[] GenerateTriangles()
     {
-        int[] triangles = new int[meshSizeX * meshSizeZ * 54];
+        int totalTriCount = 54 * meshSizeX * meshSizeZ;
+        int[] triangles = new int[totalTriCount];
 
-        triangles.AddTriangle(0, 0, 1, 4);
-        triangles.AddTriangle(3, 0, 4, 3);
-        triangles.AddTriangle(6, 1, 2, 4);
-        triangles.AddTriangle(9, 2, 5, 4);
-        triangles.AddTriangle(12, 2, 9, 5);
-        triangles.AddTriangle(15, 9, 12, 5);
-        triangles.AddTriangle(18, 3, 7, 6);
-        triangles.AddTriangle(21, 3, 4, 7);
-        triangles.AddTriangle(24, 4, 5, 8);
-        triangles.AddTriangle(27, 4, 8, 7);
-        triangles.AddTriangle(30, 5, 12, 15);
-        triangles.AddTriangle(33, 5, 15, 8);
-        triangles.AddTriangle(36, 6, 7, 93);
-        triangles.AddTriangle(39, 7, 94, 93);
-        triangles.AddTriangle(42, 7, 8, 94);
-        triangles.AddTriangle(45, 8, 95, 94);
-        triangles.AddTriangle(48, 8, 15, 102);
-        triangles.AddTriangle(51, 8, 102, 95);
+        for (int k = 0; k < meshSizeZ; k++)
+        {
+            for (int i = 0; i < meshSizeX; i++)
+            {
+                TrianglesForCell(i, k).CopyTo(triangles, k * meshSizeX * 54 + i * 54);
+            }
+        }
 
         return triangles;
     }
-    private void TrianglesForCell(int[] triangles, int x, int z)
+    private int[] TrianglesForCell(int x, int z)
     {
+        int[] cellTris = new int[54];
+        int startingIndex = meshSizeZ * 54 * z + 54 * x;
 
+
+        cellTris.AddTriangle(0, startingIndex + 0, startingIndex + 1, startingIndex + 5);
+        cellTris.AddTriangle(3, startingIndex + 0, startingIndex + 5, startingIndex + 4);
+        cellTris.AddTriangle(6, startingIndex + 1, startingIndex + 2, startingIndex + 5);
+        cellTris.AddTriangle(9, startingIndex + 5, startingIndex + 2, startingIndex + 6);
+        cellTris.AddTriangle(12, startingIndex + 2, startingIndex + 3, startingIndex + 6);
+        cellTris.AddTriangle(15, startingIndex + 6, startingIndex + 3, startingIndex + 7);
+        cellTris.AddTriangle(18, startingIndex + 4, startingIndex + 5, startingIndex + 8);
+        cellTris.AddTriangle(21, startingIndex + 8, startingIndex + 5, startingIndex + 9);
+        cellTris.AddTriangle(24, startingIndex + 5, startingIndex + 6, startingIndex + 9);
+        cellTris.AddTriangle(27, startingIndex + 9, startingIndex + 6, startingIndex + 10);
+        cellTris.AddTriangle(30, startingIndex + 6, startingIndex + 7, startingIndex + 10);
+        cellTris.AddTriangle(33, startingIndex + 10, startingIndex + 7, startingIndex + 11);
+        cellTris.AddTriangle(36, startingIndex + 8, startingIndex + 9, startingIndex + 12);
+        cellTris.AddTriangle(39, startingIndex + 12, startingIndex + 9, startingIndex + 13);
+        cellTris.AddTriangle(42, startingIndex + 9, startingIndex + 10, startingIndex + 13);
+        cellTris.AddTriangle(45, startingIndex + 13, startingIndex + 10, startingIndex + 14);
+        cellTris.AddTriangle(48, startingIndex + 10, startingIndex + 15, startingIndex + 14);
+        cellTris.AddTriangle(51, startingIndex + 10, startingIndex + 11, startingIndex + 15);
+
+        return cellTris;
     }
 
 
